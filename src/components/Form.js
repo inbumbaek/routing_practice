40d38335ea395b63fa2264f0725ba2ba66939f7b
inbumbaek <inbumbaek@gmail.com>
2023-05-22 11:29:19 -0700

    routing practice

---
+++ src/components/Form.js
@@ -0,0 +1,18 @@
+// import React, {useState} from 'react';
+// import { useParams } from 'react-router-dom'
+
+// const Form = (props) => {
+//     const {word, color, backColor} = useParams()
+
+//     return (
+//         <div>
+//             {
+//                 isNaN(word)?
+//                 <p style={{color:color, backgroundColor: backColor}}>The word is: {word}</p> :
+//                 <p>The number is: {word} </p>
+//             }
+//         </div>
+//     )
+// }
+
+// export default Form;
---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+import React, {useState} from 'react';
+
+const Home = (props) => {
+    return (
+        <div>
+            <p>Welcome</p>
+        </div>
+)}
+
+export default Home;
