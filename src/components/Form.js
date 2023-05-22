dd53bf50071ccd729037f1e3d639393e41b7338b
inbumbaek <inbumbaek@gmail.com>
2023-05-22 11:30:10 -0700

    routing update

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,18 +1,18 @@
-// import React, {useState} from 'react';
-// import { useParams } from 'react-router-dom'
+import React, {useState} from 'react';
+import { useParams } from 'react-router-dom'
 
-// const Form = (props) => {
-//     const {word, color, backColor} = useParams()
+const Form = (props) => {
+    const {word, color, backColor} = useParams()
 
-//     return (
-//         <div>
-//             {
-//                 isNaN(word)?
-//                 <p style={{color:color, backgroundColor: backColor}}>The word is: {word}</p> :
-//                 <p>The number is: {word} </p>
-//             }
-//         </div>
-//     )
-// }
+    return (
+        <div>
+            {
+                isNaN(word)?
+                <p style={{color:color, backgroundColor: backColor}}>The word is: {word}</p> :
+                <p>The number is: {word} </p>
+            }
+        </div>
+    )
+}
 
-// export default Form;
+export default Form;
